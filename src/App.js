419c3b57e77df49d349236518b52a3650b70d39d
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2023-08-30 00:27:47 +0530

    route fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { BrowserRouter ,Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import {WeatherProvider} from "./provider/showeather_provider"
-import OneDayWeatherDetails from './components/OneDayWeatherDetails';
-import { CityProvider } from './provider/city_provider';
-import WeatherTemperatureComponent from './components/WeatherTemperatureComponent';
-import {WeatherDataProvider} from '../src/provider/WeatherDataProvider'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import { WeatherProvider } from "./provider/showeather_provider";
+import OneDayWeatherDetails from "./components/OneDayWeatherDetails";
+import { CityProvider } from "./provider/city_provider";
+import WeatherTemperatureComponent from "./components/WeatherTemperatureComponent";
+import { WeatherDataProvider } from "../src/provider/WeatherDataProvider";
+
 const App = () => (
- <WeatherProvider>
-  <CityProvider>
-    <WeatherDataProvider>
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' exact element= {<HomePage/>}/>
-    <Route path='/weatherdetail' exact element= {<OneDayWeatherDetails/>}/>
-  </Routes>
-  </BrowserRouter>
-  </WeatherDataProvider>
-  </CityProvider>
-  
+  <WeatherProvider>
+    <CityProvider>
+      <WeatherDataProvider>
+        <BrowserRouter basename="/lookatweather">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/weatherdetail" element={<OneDayWeatherDetails />} />
+          </Routes>
+        </BrowserRouter>
+      </WeatherDataProvider>
+    </CityProvider>
   </WeatherProvider>
-  
 );
 
 export default App;
